f64b29eb74789f7b4d634831c7ba34b45c82dd2d
Nikita <nikitapashinsky@gmail.com>
2023-04-23 13:53:52 +0300

    render colours

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,48 @@ export async function getStaticProps() {
 }
 
 export default function Home({ colours }) {
+  console.log(colours);
+
   return (
-    <main className={`${inter.className}`}>
+    <main className={`${inter.className} p-8`}>
       Forza Colours
-      {colours.map((colour, index) => (
-        <p key={index}>{colour.colour_name}</p>
-      ))}
+      <div className="flex flex-col gap-8">
+        {colours.map((colour) => {
+          const hsl_1 = `hsl(
+            ${colour.h_1}
+            ${colour.s_1 * 100}%
+            ${colour.b_1 * 100}%
+          )`;
+
+          const hsl_2 = `hsl(
+            ${colour.h_2}
+            ${colour.s_2 * 100}%
+            ${colour.b_2 * 100}%
+          )`;
+
+          return (
+            <div key={colour.id} className="flex">
+              <div className="flex flex-col w-full">
+                <p className="text-xs">{colour.make}</p>
+                <h3 className="w-full font-semibold">{colour.colour_name}</h3>
+                <p className="text-neutral-500">{colour.paint_type}</p>
+              </div>
+              <div className="flex w-full">
+                <div
+                  className="w-full h-12"
+                  style={{ backgroundColor: hsl_1 }}
+                ></div>
+                {colour.hf_2 !== null && (
+                  <div
+                    className="w-full h-12"
+                    style={{ backgroundColor: hsl_2 }}
+                  ></div>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
